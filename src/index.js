bbcafbf35b403f4cec540f9e32e0c3e7e984f198
Sanjana Raj <sanjana.raj.25@dartmouth.edu>
2024-04-10 18:23:40 -0400

    instructions completed

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log('starting up!');
+import $ from 'jquery';
+import './style.scss';
+// Define a function to be executed
+let num = 0;
+function update() {
+  num += 1;
+  $('#main').html(`You've been on this page for ${num} seconds`);
+}
+// Call the function every second
+setInterval(update, 1000);
