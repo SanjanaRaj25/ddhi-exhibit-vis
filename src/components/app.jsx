53ff96fb3f260445bb31088efdcb9dd6b00bdb0c
Sanjana Raj <98998584+SanjanaRaj25@users.noreply.github.com>
2024-04-14 21:35:32 -0400

    Update app.jsx

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,36 +1,6 @@
-/* eslint-disable no-unused-vars */
-import React, { useCallback, useEffect, useState } from 'react';
-import debounce from 'lodash.debounce';
+import React from 'react';
 
-// import components
-import SearchBar from './search_bar';
-import youtubeSearch from '../services/youtube-api';
-import VideoList from './video_list';
-import VideoDetail from './video_detail';
-
-function App(props) {
-  const [videos, setVideos] = useState([]);
-  const [selectedVideo, setSelected] = useState(null);
-
-  const search = (text) => {
-    youtubeSearch(text).then((result) => {
-      setVideos(result);
-      setSelected(result[0]);
-    });
-  };
-
-  // create a new debounced search function
-  const debouncedSearch = useCallback(debounce(search, 500), []);
-
-  return (
-    <div>
-      <SearchBar onSearchChange={debouncedSearch} />
-      <div id="video-section">
-        <VideoDetail video={selectedVideo} />
-        <VideoList onVideoSelect={(selection) => setSelected(selection)} videos={videos} />
-      </div>
-    </div>
-  );
+  return ();
 }
 
 export default App;
