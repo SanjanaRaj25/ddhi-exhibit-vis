dc82d8c57d97204bb17dd986d306e1241c08169f
Sanjana Raj <sanjana.raj.25@dartmouth.edu>
2024-04-19 15:40:44 -0400

    routing added

--- src/components/app.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-
-  return ();
-}
-
-export default App;
--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,55 @@
-import React from 'react';
+import React, { useParams } from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './components/app';
 import './style.scss';
+import {
+  BrowserRouter, Routes, Route, NavLink,
+} from 'react-router-dom';
 
-  <App />;
+function Nav(props) {
+  return (
+    <nav>
+      <ul>
+        <li><NavLink to="/">Home</NavLink></li>
+        <li><NavLink to="/about">About</NavLink></li>
+        <li><NavLink to="/test/id1">test id1</NavLink></li>
+        <li><NavLink to="/test/id2">test id2</NavLink></li>
+
+      </ul>
+    </nav>
+  );
+}
+
+function About(props) {
+  return <div> All there is to know about me </div>;
+}
+function Welcome(props) {
+  return <div>Welcome</div>;
+}
+
+function Test(props) {
+  const { id } = useParams();
+  return <div> ID: {id} </div>;
+}
+
+function FallBack(props) {
+  return <div>URL Not Found</div>;
+}
+
+function App(props) {
+  return (
+    <BrowserRouter>
+      <div>
+        <Nav />
+        <Routes>
+          <Route path="/" element={<Welcome />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/test/:id" element={<Test />} />
+          <Route path="*" element={<FallBack />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+}
 
 const root = createRoot(document.getElementById('main'));
 root.render(<App />);
