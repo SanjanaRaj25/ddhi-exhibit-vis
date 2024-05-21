f8b1cf886121b2dc058e3173ebf29dc8b38792fc
Sanjana Raj <sanjana.raj.25@dartmouth.edu>
2024-05-21 00:07:48 -0400

    s

--- src/components/Laughlin.jsx
+++ src/components/Laughlin.jsx
@@ -33,34 +33,30 @@ function CardExampleGroups() {
       </div>
       <div className="gallery">
         {/* james laughlin */}
-        <p>1960-1970</p>
-        <div className="layer-2"><SingleCard quote="I think we all felt that sooner or later we were going to be drafted, and my personal feeling, and I think a lot of my classmates shared this, was that we would be better off serving our country as officers than as enlisted men or draftees." date="1960-1964" person="James Laughlin" align="align-5" texture="texture-1" /></div>
+        <h5 className="sub">1960-1970</h5>
+        <SingleCard quote="I think we all felt that sooner or later we were going to be drafted, and my personal feeling, and I think a lot of my classmates shared this, was that we would be better off serving our country as officers than as enlisted men or draftees." date="1960-1964" person="James Laughlin" align="align-5" texture="texture-1" />
         {/* <div className="layer-2"><SingleCard quote="I don’t think most Americans knew where the country was until long after we graduated in 1964. Even when I was in law school, I didn’t pay that much attention to what was going on in Vietnam, although what was happening was shaping a radical change in the fabric of our society and our government" date="1960" person="James Laughlin" align="align-5" texture="texture-1" /></div>
         <div className="layer-2"><SingleCard quote="I think we were very naïve in that we didn’t pay much attention to it. There was very little that was being reported back to the U.S. by the press. I think they were suppressed in what they could and could not send back." date="1964-1967" person="James Laughlin" align="align-6" texture="texture-2" /></div> */}
         {/* <div className="layer-2"><SingleCard quote="They were neutral. They didn’t express much of an opinion one way or the other, although when I finally got sent to Vietnam in 1967, they were adamantly opposed to the war. As was I." date="1967" person="James Laughlin" align="align-2" texture="texture-2" /></div> */}
-        <div className="layer-2"><SingleCard quote="I never really thought about it until I was ordered to active duty at Fort Benning, Georgia. And from the day we got there, all that people talked about was Vietnam. I said to myself, What are they so concerned about? It’s just a tiny little conflict in a tiny little country in Indochina, what used to be French Indochina." date="1967" person="James Laughlin" align="align-6" texture="texture-2" /></div>
-        <div className="layer-2"><SingleCard quote="I think what was being filtered back to the American public in 1967, when I went on active duty, was a sanitized version of what was actually taking place and that these troop buildups that we were experiencing, which were supposed to be temporary, became permanent and more bloody as time went on." date="1967" person="James Laughlin" align="align-3" texture="texture-2" /></div>
-        <div className="layer-2"> <SingleCard quote="The first time that I really sat up and said, Whoa, there’s something wrong here was when we got hit with the Tet Offensive in 1968, the first time we were under heavy enemy attack. " date="1968" person="James Laughlin" align="align-3" texture="texture-2" /></div>
+        <SingleCard quote="I never really thought about it until I was ordered to active duty at Fort Benning, Georgia. And from the day we got there, all that people talked about was Vietnam. I said to myself, What are they so concerned about? It’s just a tiny little conflict in a tiny little country in Indochina, what used to be French Indochina." date="1967" person="James Laughlin" align="align-6" texture="texture-2" />
+        <SingleCard quote="I think what was being filtered back to the American public in 1967, when I went on active duty, was a sanitized version of what was actually taking place and that these troop buildups that we were experiencing, which were supposed to be temporary, became permanent and more bloody as time went on." date="1967" person="James Laughlin" align="align-3" texture="texture-2" />
+        <SingleCard quote="The first time that I really sat up and said, Whoa, there’s something wrong here was when we got hit with the Tet Offensive in 1968, the first time we were under heavy enemy attack. " date="1968" person="James Laughlin" align="align-3" texture="texture-2" />
         {/* <div className="layer-2"><SingleCard quote="I think that the American soldier, by and large, did a remarkable job of fighting under very adverse conditions in a very ferocious war." date="1968" person="James Laughlin" align="align-5" texture="texture-2" /></div> */}
 
-        <p>2015-present</p>
-        <div className="layer-5"><SingleCard quote="I always felt that—and I feel to this day—that having liberal arts students from schools like Dartmouth or Cornell or NYU [New York University] adds a leavening experience to the military that you don’t get from having your officer corps comprised solely of West Point graduates." date="2015" person="James Laughlin" align="align-7" texture="texture-5" /></div>
-        <div className="layer-2"><SingleCard quote="The whole operational plan was flawed. I don’t think that, given the constraints under which we were operating, that the war was winnable." date="2015" person="James Laughlin" align="align-3" texture="texture-5" /></div>
-        <div className="layer-2"><SingleCard quote=" It didn’t take long for those of us who were sent there to realize that we were pawns who really had little control over what we were supposed to be doing, and the name of the game was to survive and help your buddies survive and get home in one piece." date="2015" person="James Laughlin" align="align-1" texture="texture-5" /></div>
-        <div className="layer-2"><SingleCard quote="I thought the whole experience, both my personal role and the role of the American armed forces, was a total waste in, first of all, valuable American lives, capital, and in general, in terms of how it affected the fabric of this country and our society, it had an overwhelming impact
+        <h5 className="sub">2015-present</h5>
+        <SingleCard quote="I always felt that—and I feel to this day—that having liberal arts students from schools like Dartmouth or Cornell or NYU [New York University] adds a leavening experience to the military that you don’t get from having your officer corps comprised solely of West Point graduates." date="2015" person="James Laughlin" align="align-7" texture="texture-5" />
+        <SingleCard quote="The whole operational plan was flawed. I don’t think that, given the constraints under which we were operating, that the war was winnable." date="2015" person="James Laughlin" align="align-3" texture="texture-5" />
+        <SingleCard quote=" It didn’t take long for those of us who were sent there to realize that we were pawns who really had little control over what we were supposed to be doing, and the name of the game was to survive and help your buddies survive and get home in one piece." date="2015" person="James Laughlin" align="align-1" texture="texture-5" />
+        <SingleCard quote="I thought the whole experience, both my personal role and the role of the American armed forces, was a total waste in, first of all, valuable American lives, capital, and in general, in terms of how it affected the fabric of this country and our society, it had an overwhelming impact
 "
           date="2015"
           person="James Laughlin"
           align="align-1"
           texture="texture-5"
-        /></div>
-        <div className="I think that, from my own personal feelings, I’m proud of the fact that I served and the men that served with me. I regret that we were involved in a conflict that was so unpopular and didn’t really produce any positive results."><SingleCard quote="d" date="2015" person="James Laughlin" align="align-1" texture="texture-5" /></div>
-        <div className="I was diagnosed with Parkinson’s disease in 2006. Around 2008 or 2009, that disease got added to the list of conditions presumptively caused by exposure to Agent Orange, by the Veterans Administration, which was the first time I really came to grips with it and said, Hey, wow! Now I understand, to a certain extent, how this happened to me.
-"
-        ><SingleCard quote="d" date="2015" person="James Laughlin" align="align-1" texture="texture-5" /></div>
-
+        />
+        <SingleCard quote="I think that, from my own personal feelings, I’m proud of the fact that I served and the men that served with me. I regret that we were involved in a conflict that was so unpopular and didn’t really produce any positive results." date="2015" person="James Laughlin" align="align-7" texture="texture-5" />
+        <SingleCard quote="I was diagnosed with Parkinson’s disease in 2006. Around 2008 or 2009, that disease got added to the list of conditions presumptively caused by exposure to Agent Orange, by the Veterans Administration, which was the first time I really came to grips with it and said, Hey, wow! Now I understand, to a certain extent, how this happened to me." date="2015" person="James Laughlin" align="align-7" texture="texture-5" />
       </div>
-
     </div>
   );
 }
